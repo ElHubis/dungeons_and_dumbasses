2bf6a91f6689d6e62c83473a0b92c1760048c6bc
Ruben <rubenmancos@gmail.com>
2023-02-27 12:20:24 +0100

    Egna färger för varje karaktär

--- char.js
+++ char.js
@@ -9,6 +9,22 @@ const handleOnMouseMove = e =>{
     target.style.setProperty("--mouse-y", `${y}px`)
 }
 
-for(const char of document.querySelectorAll(".char")){
-    char.onmousemove = e => handleOnMouseMove(e);
+for(const one of document.querySelectorAll(".one")){
+    one.onmousemove = e => handleOnMouseMove(e);
+
+}
+
+for(const two of document.querySelectorAll(".two")){
+    two.onmousemove = e => handleOnMouseMove(e);
+
+}
+
+for(const three of document.querySelectorAll(".three")){
+    three.onmousemove = e => handleOnMouseMove(e);
+
+}
+
+for(const four of document.querySelectorAll(".four")){
+    four.onmousemove = e => handleOnMouseMove(e);
+
 }
