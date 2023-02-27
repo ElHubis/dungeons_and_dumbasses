9bc2b887a5acea44d0f86844fc4ff4fd6daf8191
Ruben <rubenmancos@gmail.com>
2023-02-27 14:36:55 +0100

    Flertal änringar
    Testade en ny bakgrund på character page
    Försökte använde en ny metod för hover effekten på character page
    Lade till favicon(temporär)

--- char.js
+++ char.js
@@ -1,30 +1,27 @@
-const handleOnMouseMove = e =>{
-    const { currentTarget: target} = e;
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
 
     const rect = target.getBoundingClientRect(),
-        x = e.clientX -rect.left,
-        y = e.clientY -rect.top
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top
 
     target.style.setProperty("--mouse-x", `${x}px`)
     target.style.setProperty("--mouse-y", `${y}px`)
 }
 
-for(const one of document.querySelectorAll(".one")){
-    one.onmousemove = e => handleOnMouseMove(e);
+for (const char of document.querySelectorAll(".char")) {
+    char.onmousemove = e => handleOnMouseMove(e);
 
 }
 
-for(const two of document.querySelectorAll(".two")){
-    two.onmousemove = e => handleOnMouseMove(e);
+const profiles = document.querySelectorAll(".char")
+const colors = [
+    "rgba(24, 33, 215, 0.3)",
+    "rgba(170, 6, 179, 0.3),",
+    "rgba(179, 6, 93, 0.3)",
+    "rgba(12, 141, 20, 0.3)",
+]
 
-}
-
-for(const three of document.querySelectorAll(".three")){
-    three.onmousemove = e => handleOnMouseMove(e);
-
-}
-
-for(const four of document.querySelectorAll(".four")){
-    four.onmousemove = e => handleOnMouseMove(e);
-
+for (let index = 0; index < profiles.length; index++) {
+    element.style.background = `radial-gradient(250px circle at var(--mouse-x) var(--mouse-y), ${colors[index]}, transparent 40%)`
 }
