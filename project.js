b689ed0fcccb26452fdb3505fc256892572746b8
Ruben <rubenmancos@gmail.com>
2023-03-07 16:58:18 +0100

    Jobbar med timeline och mainpage
    Co-Authored-By: NTeskedI <121166969+NTeskedI@users.noreply.github.com>

---
+++ project.js
@@ -0,0 +1,30 @@
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top
+
+    target.style.setProperty("--mouse-x", `${x}px`)
+    target.style.setProperty("--mouse-y", `${y}px`)
+}
+
+for (const one of document.querySelectorAll(".one")) {
+    one.onmousemove = e => handleOnMouseMove(e);
+
+}
+
+for (const two of document.querySelectorAll(".two")) {
+    two.onmousemove = e => handleOnMouseMove(e);
+
+}
+
+for (const three of document.querySelectorAll(".three")) {
+    three.onmousemove = e => handleOnMouseMove(e);
+
+}
+
+for (const four of document.querySelectorAll(".four")) {
+    four.onmousemove = e => handleOnMouseMove(e);
+
+}
